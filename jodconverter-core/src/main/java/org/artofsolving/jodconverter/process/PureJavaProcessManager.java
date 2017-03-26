3c0dcc8791d26b9efab42b85e4482773141dcf3b
Paul Chapman <chapman@zemsys.com>
2017-03-26 14:35:28 +1100

    Use kill/taskkill if no process object

--- jodconverter-core/src/main/java/org/artofsolving/jodconverter/process/PureJavaProcessManager.java
+++ jodconverter-core/src/main/java/org/artofsolving/jodconverter/process/PureJavaProcessManager.java
@@ -12,14 +12,41 @@
 //
 package org.artofsolving.jodconverter.process;
 
+import java.io.IOException;
+import java.util.List;
+
+import org.apache.commons.io.IOUtils;
+import org.artofsolving.jodconverter.office.OfficeException;
+import org.artofsolving.jodconverter.util.PlatformUtils;
+
 public class PureJavaProcessManager implements ProcessManager {
 
-    public long findPid(ProcessQuery query) {
-        return PID_UNKNOWN;
-    }
+	public long findPid(ProcessQuery query) {
+		return PID_UNKNOWN;
+	}
 
-    public void kill(Process process, long pid) {
-        process.destroy();
-    }
+	public void kill(Process process, long pid) {
 
+		if (process == null) {
+			String processId = String.valueOf(pid);
+			if (PlatformUtils.isWindows())
+				execute(pid, "taskkill", "/pid", processId, "/f");
+			else
+				execute(pid, "/bin/kill", "-KILL", processId);
+		} else {
+			process.destroy();
+		}
+	}
+
+	private List<String> execute(long pid, String... args) {
+		try {
+			String[] command = args;
+			Process process = new ProcessBuilder(command).start();
+			@SuppressWarnings("unchecked")
+			List<String> lines = IOUtils.readLines(process.getInputStream());
+			return lines;
+		} catch (IOException e) {
+			throw new OfficeException("Error trying to kill process " + pid);
+		}
+	}
 }
